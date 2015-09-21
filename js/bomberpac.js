41ace7728bc9b3a6443420ea8fd928ca3236652e
crodeheaver <colbyrodeheaver@hotmail.com>
2015-09-21 21:46:46 +0000

    EXPLOSIONS

--- js/bomberpac.js
+++ js/bomberpac.js
@@ -24,8 +24,8 @@ var Bomberpac = function(game) {
 }
 
 Bomberpac.prototype = {
-    fireRate : 1000,
-    nextFire : 0,
+    fireRate: 1000,
+    nextFire: 0,
     init: function() {
 
         this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
@@ -39,8 +39,8 @@ Bomberpac.prototype = {
     },
     preload: function() {
         this.load.image('dot', 'assets/dot.png');
-        this.load.spritesheet('explosion', 'assets/explosions.png',16,16,35,0,1);
-        
+        this.load.spritesheet('explosion', 'assets/explosions.png', 16, 16, 35, 0, 1);
+
         game.load.image('tiles', 'assets/pacman-tiles.png');
         this.load.tilemap('map', 'assets/pacman-map.json', null, Phaser.Tilemap.TILED_JSON);
         game.load.spritesheet('bomberman', 'assets/bomberman.png', 35, 52);
@@ -50,43 +50,92 @@ Bomberpac.prototype = {
     },
 
     create: function() {
-        
+
         this.map = this.add.tilemap('map');
         this.map.addTilesetImage('pacman-tiles', 'tiles');
         this.layer = this.map.createLayer('Pacman');
-        
+
         //  Our bomb group
         this.bombs = game.add.group();
         this.bombs.enableBody = true;
         this.bombs.physicsBodyType = Phaser.Physics.ARCADE;
         this.bombs.createMultiple(3, 'bomb');
         //this.bombs.scale.setTo(.8,.8);
-        this.bombs.setAll('scale.x',.8);
-        this.bombs.setAll('scale.y',.8);
+        this.bombs.setAll('scale.x', .8);
+        this.bombs.setAll('scale.y', .8);
         this.bombs.setAll('anchor.x', 0.5);
         this.bombs.setAll('anchor.y', 0.5);
         this.bombs.setAll('outOfBoundsKill', true);
         this.bombs.setAll('checkWorldBounds', true);
-        
-        //explosion group
-        this.explosions = game.add.group();
-        this.explosions.enableBody = true;
-        this.explosions.physicsBodyType = Phaser.Physics.ARCADE;
-        this.explosions.createMultiple(3,'explosion',5);
-        this.explosions.setAll('scale.x',1.5);
-        this.explosions.setAll('scale.y',1.5);
-        this.explosions.setAll('anchor.x', 0.5);
-        this.explosions.setAll('anchor.y', 0.5);
-        this.explosions.setAll('outOfBoundsKill', true);
-        this.explosions.setAll('checkWorldBounds', true);
-        
+
+        //center explosion group
+        this.centerExplosion = game.add.group();
+        this.centerExplosion.enableBody = true;
+        this.centerExplosion.physicsBodyType = Phaser.Physics.ARCADE;
+        this.centerExplosion.createMultiple(3, 'explosion', 5);
+        this.centerExplosion.setAll('scale.x', 1.5);
+        this.centerExplosion.setAll('scale.y', 1.5);
+        this.centerExplosion.setAll('anchor.x', 0.5);
+        this.centerExplosion.setAll('anchor.y', 0.5);
+        this.centerExplosion.setAll('outOfBoundsKill', true);
+        this.centerExplosion.setAll('checkWorldBounds', true);
+
+        //right explosion group
+        this.rightExplosion = game.add.group();
+        this.rightExplosion.enableBody = true;
+        this.rightExplosion.physicsBodyType = Phaser.Physics.ARCADE;
+        this.rightExplosion.createMultiple(3, 'explosion', 2);
+        this.rightExplosion.setAll('scale.x', 1.5);
+        this.rightExplosion.setAll('scale.y', 1.5);
+        this.rightExplosion.setAll('anchor.x', 0.5);
+        this.rightExplosion.setAll('anchor.y', 0.5);
+        this.rightExplosion.setAll('outOfBoundsKill', true);
+        this.rightExplosion.setAll('checkWorldBounds', true);
+
+        //left explosion group
+        this.leftExplosion = game.add.group();
+        this.leftExplosion.enableBody = true;
+        this.leftExplosion.physicsBodyType = Phaser.Physics.ARCADE;
+        this.leftExplosion.createMultiple(3, 'explosion', 7);
+        this.leftExplosion.setAll('scale.x', 1.5);
+        this.leftExplosion.setAll('scale.y', 1.5);
+        this.leftExplosion.setAll('anchor.x', 0.5);
+        this.leftExplosion.setAll('anchor.y', 0.5);
+        this.leftExplosion.setAll('outOfBoundsKill', true);
+        this.leftExplosion.setAll('checkWorldBounds', true);
+
+        //top explosion group
+        this.topExplosion = game.add.group();
+        this.topExplosion.enableBody = true;
+        this.topExplosion.physicsBodyType = Phaser.Physics.ARCADE;
+        this.topExplosion.createMultiple(3, 'explosion', 8);
+        this.topExplosion.setAll('scale.x', 1.5);
+        this.topExplosion.setAll('scale.y', 1.5);
+        this.topExplosion.setAll('anchor.x', 0.5);
+        this.topExplosion.setAll('anchor.y', 0.5);
+        this.topExplosion.setAll('outOfBoundsKill', true);
+        this.topExplosion.setAll('checkWorldBounds', true);
+
+        //bottom explosion group
+        this.bottomExplosion = game.add.group();
+        this.bottomExplosion.enableBody = true;
+        this.bottomExplosion.physicsBodyType = Phaser.Physics.ARCADE;
+        this.bottomExplosion.createMultiple(3, 'explosion', 0);
+        this.bottomExplosion.setAll('scale.x', 1.5);
+        this.bottomExplosion.setAll('scale.y', 1.5);
+        this.bottomExplosion.setAll('anchor.x', 0.5);
+        this.bottomExplosion.setAll('anchor.y', 0.5);
+        this.bottomExplosion.setAll('outOfBoundsKill', true);
+        this.bottomExplosion.setAll('checkWorldBounds', true);
+
+        //dots
         this.dots = this.add.physicsGroup();
         //  The dots will need to be offset by 6px to put them back in the middle of the grid
         this.dots.setAll('x', 6, false, false, 1);
         this.dots.setAll('y', 6, false, false, 1);
-        
+
         this.map.createFromTiles(7, this.safetile, 'dot', this.layer, this.dots);
-        
+
         this.player = game.add.sprite(220, 280, 'bomberman', 0);
         this.player.scale.setTo(.8, .8);
         this.player.animations.add('left', [0, 1, 0, 2], 10, true);
@@ -104,7 +153,7 @@ Bomberpac.prototype = {
 
         this.fireButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 
-        
+
 
 
     },
@@ -117,30 +166,59 @@ Bomberpac.prototype = {
         }
 
     },
-    explodeBomb: function(){
-        
+    explodeBomb: function() {
         this.kill();
+
     },
-    createExplosion: function(){
-        this.reset(220,280);
+    createExplosion: function() {
+        this.center.reset(this.player.x, this.player.y);
+
+        this.left.reset(this.player.x - 23, this.player.y);
+        this.right.reset(this.player.x + 23, this.player.y);
+
+        this.top.reset(this.player.x, this.player.y + 23);
+        this.bottom.reset(this.player.x, this.player.y - 23);
     },
-    
+    destroyExplosion: function() {
+        this.center.kill();
+
+        this.left.kill();
+        this.right.kill();
+
+        this.top.kill();
+        this.bottom.kill();
+    },
+
     fire: function() {
-        
-        
+
+
         if (game.time.now > this.nextFire && this.bombs.countDead() > 0) {
             this.nextFire = game.time.now + this.fireRate;
-            
+
             var bomb = this.bombs.getFirstExists(false);
             //debugger;
-            var bombx = this.player.x, bomby = this.player.y;
-            bomb.reset(bombx,bomby);
-            
+            var bombx = this.player.x,
+                bomby = this.player.y;
+            bomb.reset(bombx, bomby);
+            var explosion = {
+                center: this.centerExplosion.getFirstExists(false),
+                left: this.leftExplosion.getFirstExists(false),
+                right: this.rightExplosion.getFirstExists(false),
+                top: this.topExplosion.getFirstExists(false),
+                bottom: this.bottomExplosion.getFirstExists(false),
+                player: {
+                    x: this.player.x,
+                    y: this.player.y
+                }
+            };
+
+
             game.time.events.add(Phaser.Timer.SECOND * 3, this.explodeBomb, bomb);
-            game.time.events.add(Phaser.Timer.SECOND * 3, this.createExplosion, this.explosions.getFirstExists(false));
-            
+            game.time.events.add(Phaser.Timer.SECOND * 3, this.createExplosion, explosion);
+            game.time.events.add(Phaser.Timer.SECOND * 4.5, this.destroyExplosion, explosion);
+
         }
-        
+
     },
 
     update: function() {
